3e6bcd90f287bc29f8f7612d1bb0da9c2ebee64a
Chirag Baghasingh <chirag2bsingh@gmail.com>
2022-01-26 23:27:51 +0530

    app initialised

---
+++ config.js
@@ -0,0 +1,3 @@
+export const ADDRESS = ""//Paste address here
+export const DECIMALS = //The number of decimals you chose when deploying the contract
+export const ABI = //You can get this from Remix, in the Solidity Compiler tab.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/TokenInfo/Header';
+import GetBalance from './components/TokenInfo/GetBalance';
+import BuyForm from './components/PersonalPanel/BuyForm';
+import TransferForm from './components/PersonalPanel/TransferForm';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+     <GetBalance/>
+     <div>Wanna transfer?</div>
+     <TransferForm/>
+     <div>Wanna buy?</div>
+     <BuyForm/>
     </div>
   );
 }
